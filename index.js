7d94634d4eaef98831a10cc9431d4e802ff748a6
Amjad137 <mohammedamjath137@gmail.com>
2023-03-23 02:16:04 +0530

    Initiated

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const {users} = require("./data/users.json");
+
+const userRouter = require("./routes/user.js");
+
+const app = express();
+app.use(express.json());
+
+const port=8081;
+
+
+app.get("/", (req,res) => {
+    res.status(200).send({
+        Message: "hello"
+    });
+});
+
+app.use("/users", userRouter);
+
+//to manage invalid routes (this should be kept on very low area)
+app.get("*", (req,res) => {
+    res.status(404).send({
+        Message : "This route Doesn't Exist"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`The Server is Running on ${port}`);
+});
+
+//http/localhost:8081
---
+++ routes/user.js
@@ -0,0 +1,126 @@
+const express = require("express");
+
+const {users} = require("../data/users.json");
+
+const router =express.Router();
+
+/**
+ * Route: /users
+ * Method: GET
+ * description: Get All Users
+ * Access: Public
+ * Parameter:none
+ */
+
+router.get("/", (req,res) => {
+    res.status(200).json({ //can't use send for more lines of output
+        success: true,
+        data: users,
+    });
+});
+
+/**
+ * Route: /users/:id
+ * Method: GET
+ * description: Get User by ID
+ * Access: Public
+ * Parameter: id
+ */
+
+router.get("/:id", (req,res) => { 
+    
+    const {id} = req.params;
+    const user = users.find((each) => each.id === id);
+
+    if (!user){
+      return res.status(404).json({
+        success: false,
+        Message: "User Not Found",
+     });
+    }
+
+    return  res.status(200).json({
+    success: true,
+    Message: "User Found",
+    data: user,
+    });
+
+});
+  
+
+/**
+ * Route: /users
+ * Method: POST
+ * description: Creating a New User
+ * Access: Public
+ * Parameter: none
+ */
+
+router.post("/", (req,res) => {
+    const {id, name, surname, email, subscriptionType, subscriptionDate} = req.body;
+
+    const user = users.find((each) => each.id === id);
+    if (user){
+        return res.status(404).json({
+            success: false,
+            Message: "User Already Exist",
+        });
+    }
+
+    users.push({
+        id, 
+        name, 
+        surname, 
+        email, 
+        subscriptionType, 
+        subscriptionDate,
+    });
+    return res.status(200).json({
+        success: true,
+        Message: "User Added",
+        data: users,
+    });
+});
+
+/**
+ * Route: /users/:id
+ * Method: PUT
+ * description: Updating a User by ID
+ * Access: Public
+ * Parameter: id
+ */
+
+router.put("/:id",(req,res)=>{
+    const {id} = req.params;
+    const {data} = req.body;
+
+    const user = users.find((each) => each.id ===id);
+    if (!user){
+        return res.status(404).json({
+            success: false,
+            Message: "User Not Found"
+        });
+    }
+
+    const UpdatedUserData = users.map((each) => {
+        if (each.id === id){
+            return{ 
+            ...each,
+            ...data
+        };
+        }
+        return each;
+    });
+
+    return res.status(200).json({
+        success: true,
+        Message: "Updated",
+        data: UpdatedUserData,
+    });
+});
+
+
+
+
+
+module.exports=router;
